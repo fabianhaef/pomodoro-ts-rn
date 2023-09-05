4b36e37fcf98f8a243f06dd745abbd49eb5bd624
fabianhaef <fabian.haefi@gmail.com>
2023-09-05 20:19:20 +0200

    add isTimerRunning state to remove one button element

--- App.tsx
+++ App.tsx
@@ -8,29 +8,28 @@ const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
 
 export default function App() {
   const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
-  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null)
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null);
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
 
   const startTimer = () => {
+    setIsTimerRunning(true)
     const id = setInterval(() => setTimerCount(prev => prev - 1000), 1000);
     setTimerInterval(id)
   }
 
   const stopTimer = () => {
+    setIsTimerRunning(false)
     if (timerInterval !== null) {
       clearInterval(timerInterval);
     }
   };
 
-  // create a new date object to extract minutes and seconds
-  const timerDate = new Date(timerCount);
-
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button title="Start timer" onPress={startTimer} />
-      <Button title="Stop timer" onPress={stopTimer} />
-      <TierCountDownDisplay timerDate={timerDate} />
+      <Button title={isTimerRunning ? 'Stop Timer' : 'Start Timer'} onPress={isTimerRunning ? stopTimer : startTimer} />
+      <TierCountDownDisplay timerDate={new Date(timerCount)} />
     </View>
   );
 }
