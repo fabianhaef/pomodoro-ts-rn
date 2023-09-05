f241e04a7da087a5372aef607617437ebe363bde
fabianhaef <fabian.haefi@gmail.com>
2023-09-05 20:48:57 +0200

    refactor App.tsx, add timerMode state

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TimerCountDownDisplay from './TimerCountDownDisplay';
 import TimerToggleButton from './TimerToggleButton';
 
-const FOCUS_TIME_MINUTES = 2 * 60 * 1000
+const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000
 const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
 
 export default function App() {
   const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
   const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null);
   const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+  const [timerMode, setTimerMode] = useState<'Focus' | 'Break'>('Focus');
+
+  useEffect(() => {
+    if (timerCount === 0) {
+      if (timerMode === 'Focus') {
+        setTimerMode('Break');
+        setTimerCount(BREAK_TIME_MINUTES);
+      } else {
+        setTimerMode('Focus')
+        setTimerCount(FOCUS_TIME_MINUTES);
+      }
+      stopTimer();
+    }
+  }, [timerCount])
 
   const startTimer = () => {
     setIsTimerRunning(true)
@@ -26,8 +40,9 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{ ...styles.container, ...{ backgroundColor: timerMode === 'Break' ? '#2a9d8f' : '#F1796F' } }}>
       <StatusBar style="auto" />
+      <Text>{timerMode} Time {timerMode === 'Focus' ? '🍅' : '🥦'}</Text>
       <TimerToggleButton isTimerRunning={isTimerRunning} startTimer={startTimer} stopTimer={stopTimer} />
       <TimerCountDownDisplay timerDate={new Date(timerCount)} />
     </View>
@@ -38,7 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FF3928',
     alignItems: 'center',
     justifyContent: 'center',
   },
