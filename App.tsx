aadbe5d8ef2429a8a9a82674b0f7dcdd21100f0e
fabianhaef <fabian.haefi@gmail.com>
2023-09-05 20:02:50 +0200

    create timer states and basic start and stop funcs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000
+const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
 
 export default function App() {
+  const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null)
+
+  const startTimer = () => {
+    const id = setInterval(() => setTimerCount(prev => prev - 1000), 1000);
+    setTimerInterval(id)
+  }
+
+  const stopTimer = () => {
+    if (timerInterval !== null) {
+      clearInterval(timerInterval);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Start timer" onPress={startTimer} />
+      <Button title="Stop timer" onPress={stopTimer} />
+      <Text>{timerCount}</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
